58f080ec8277d809deb138f7a0f594635b1f2cb5
Lego <legoinventor0@gmail.com>
2024-08-02 23:35:09 +0000

    Final Push

--- src/main/java/com/yourname/yourplugin/FuelMasterMain.java
+++ src/main/java/com/yourname/yourplugin/FuelMasterMain.java
@@ -1,15 +1,31 @@
-package com.blockmasters.yourplugin;
+package com.blockmasters.FuelMasterMain;
 
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.FurnaceBurnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class FuelMasterMain extends JavaPlugin {
+public class FuelMasterMain extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
-        getLogger().info("YourPlugin has been enabled!");
+        getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("FuelMaster has been enabled");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("YourPlugin has been disabled.");
+        getLogger().info("FuelMaster has been disabled");
+    }
+
+    @EventHandler
+    public void onFurnaceBurn(FurnaceBurnEvent event) {
+        if (event.getFuel() != null && event.getFuel().getType() != Material.AIR) {
+            if (event.getFuel().hasItemMeta() && event.getFuel().getItemMeta().hasEnchants()) {
+                event.setCancelled(true);
+                event.getBlock().getWorld().createExplosion(event.getBlock().getLocation(), 0); // Optional: Add some feedback
+            }
+        }
     }
 }
